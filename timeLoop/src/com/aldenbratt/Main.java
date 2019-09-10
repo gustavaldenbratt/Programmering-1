5a97dc3a530220b25b4f8b90a78d943f72227eb0
gustavaldenbratt <gustav.aldenbratt@elev.ga.ntig.se>
2019-09-10 14:57:00 +0200

    Create Main.java

---
+++ timeLoop/src/com/aldenbratt/Main.java
@@ -0,0 +1,22 @@
+package com.aldenbratt;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Number");
+        int number = input.nextInt();
+        String text = " Abracadabra";
+        if (number <= 100) {
+            for (int number2 = 1; number2 <= number; ++number2) {
+                System.out.println(number2 + text);
+            }
+        }
+        else if (number >100){
+            System.out.println("Nummret är för högt");
+        }
+    }
+}
+
+
